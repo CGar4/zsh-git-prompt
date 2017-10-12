f370833bc770c8ebba19a801fd306904d57202fb
root <root@localhost.localdomain>
2017-10-12 12:52:40 -0500

    uploading custom zshrc.sh and renaming the original.

--- origzshrc.sh
+++ origzshrc.sh
@@ -1,4 +1,5 @@
 # To install source this file from your .zshrc file
+
 # see documentation at http://linux.die.net/man/1/zshexpn
 # A: finds the absolute path, even if this is symlinked
 # h: equivalent to dirname
@@ -61,35 +62,47 @@ function update_current_git_vars() {
 
 
 git_super_status() {
-    precmd_update_git_vars
+	precmd_update_git_vars
     if [ -n "$__CURRENT_GIT_STATUS" ]; then
-	  STATUS="$ZSH_THEME_GIT_PROMPT_PREFIX$ZSH_THEME_GIT_PROMPT_BRANCH_PREFIX%F{yellow}$GIT_BRANCH $ZSH_THEME_GIT_PROMPT_BRANCH_SUFFIX%{${reset_color}%}"
+	  STATUS="$ZSH_THEME_GIT_PROMPT_PREFIX$ZSH_THEME_GIT_PROMPT_BRANCH$GIT_BRANCH%{${reset_color}%}"
 	  if [ "$GIT_BEHIND" -ne "0" ]; then
-		  STATUS="$ZSH_THEME_GIT_PROMPT_BEHIND%{${reset_color}%}$STATUS"
+		  STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_BEHIND$GIT_BEHIND%{${reset_color}%}"
 	  fi
 	  if [ "$GIT_AHEAD" -ne "0" ]; then
-		  STATUS="$ZSH_THEME_GIT_PROMPT_AHEAD%{${reset_color}%}$STATUS"
+		  STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_AHEAD$GIT_AHEAD%{${reset_color}%}"
 	  fi
-	  STATUS="$ZSH_THEME_GIT_PROMPT_SEPARATOR$STATUS"
+	  STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_SEPARATOR"
 	  if [ "$GIT_STAGED" -ne "0" ]; then
-		  STATUS="$ZSH_THEME_GIT_PROMPT_STAGED$GIT_STAGED%{${reset_color}%}$STATUS"
+		  STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_STAGED$GIT_STAGED%{${reset_color}%}"
 	  fi
 	  if [ "$GIT_CONFLICTS" -ne "0" ]; then
-		  STATUS="$ZSH_THEME_GIT_PROMPT_CONFLICTS$GIT_CONFLICTS%{${reset_color}%}$STATUS"
+		  STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_CONFLICTS$GIT_CONFLICTS%{${reset_color}%}"
 	  fi
 	  if [ "$GIT_CHANGED" -ne "0" ]; then
-		  STATUS="$ZSH_THEME_GIT_PROMPT_CHANGED%{${reset_color}%}$STATUS"
+		  STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_CHANGED$GIT_CHANGED%{${reset_color}%}"
 	  fi
 	  if [ "$GIT_UNTRACKED" -ne "0" ]; then
-		  STATUS="$ZSH_THEME_GIT_PROMPT_UNTRACKED%{${reset_color}%}$STATUS"
+		  STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_UNTRACKED%{${reset_color}%}"
 	  fi
 	  if [ "$GIT_CHANGED" -eq "0" ] && [ "$GIT_CONFLICTS" -eq "0" ] && [ "$GIT_STAGED" -eq "0" ] && [ "$GIT_UNTRACKED" -eq "0" ]; then
-		  STATUS="$ZSH_THEME_GIT_PROMPT_CLEAN$STATUS"
+		  STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_CLEAN"
 	  fi
-	  STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_SUFFIX%{${reset_color}%}"
+	  STATUS="$STATUS%{${reset_color}%}$ZSH_THEME_GIT_PROMPT_SUFFIX"
 	  echo "$STATUS"
 	fi
 }
 
+# Default values for the appearance of the prompt. Configure at will.
+ZSH_THEME_GIT_PROMPT_PREFIX="("
+ZSH_THEME_GIT_PROMPT_SUFFIX=")"
+ZSH_THEME_GIT_PROMPT_SEPARATOR="|"
+ZSH_THEME_GIT_PROMPT_BRANCH="%{$fg_bold[magenta]%}"
+ZSH_THEME_GIT_PROMPT_STAGED="%{$fg[red]%}%{●%G%}"
+ZSH_THEME_GIT_PROMPT_CONFLICTS="%{$fg[red]%}%{✖%G%}"
+ZSH_THEME_GIT_PROMPT_CHANGED="%{$fg[blue]%}%{✚%G%}"
+ZSH_THEME_GIT_PROMPT_BEHIND="%{↓%G%}"
+ZSH_THEME_GIT_PROMPT_AHEAD="%{↑%G%}"
+ZSH_THEME_GIT_PROMPT_UNTRACKED="%{…%G%}"
+ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg_bold[green]%}%{✔%G%}"
 
 
